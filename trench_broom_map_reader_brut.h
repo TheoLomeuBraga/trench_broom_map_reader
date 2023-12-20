4d06ffff334aadac4b3369fd2d00cc954f21df19
TheoLomeuBraga <theolomeu@gmail.com>
2023-12-19 22:20:16 -0300

    a

--- trench_broom_map_reader_brut.h
+++ trench_broom_map_reader_brut.h
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-//basics
-typedef struct {
+// basics
+typedef struct
+{
     void **data;
     size_t size;
 } simple_vector;
 
-typedef struct {
+typedef struct
+{
     int x;
     int y;
     int z;
 } PrimitiveCoord;
 
-typedef struct {
+typedef struct
+{
 
     int offsetX;
     int offsetY;
@@ -24,66 +27,58 @@ typedef struct {
 
 } PrimitiveTextureCoord;
 
-typedef struct {
+typedef struct
+{
 
     const char *key;
     const char *data;
 
 } MapPropriety;
 
-typedef struct {
-    
+typedef struct
+{
+
     PrimitiveCoord positions[3];
     const char *texture;
     PrimitiveTextureCoord textureCord;
-    
+
 } PrimitiveMapStructurePrimitive;
 
-//map basics
+// map basics
 
-typedef struct {
-    
-    PrimitiveCoord position;
-    MapPropriety *aditionalInfo;
-    
-} PrimitiveMapEntity;
+typedef struct
+{
 
-typedef struct {
-    
     PrimitiveMapStructurePrimitive primitives;
     simple_vector aditionalInfo;
-    
-} PrimitiveMapStructure;
 
-typedef struct {
-    
+} PrimitiveMapEntity;
+
+typedef struct
+{
+
     const char *path;
     simple_vector entitys;
-    simple_vector structures;
     simple_vector aditionalInfo;
-    
+
 } PrimitiveMap;
 
-simple_vector load_primitive_map_entitys(FILE *file){
-    simple_vector ret;
-    return ret;
-}
 
-simple_vector load_primitive_map_tructures(FILE *file){
-    simple_vector ret;
-    return ret;
-}
 
-simple_vector load_primitive_map_aditionalInfo(FILE *file) {
+
+simple_vector load_primitive_map_aditionalInfo(FILE *file)
+{
     simple_vector aditionalInfo;
     aditionalInfo.data = NULL;
     aditionalInfo.size = 0;
 
     char line[256];
 
-    while (fgets(line, sizeof(line), file) != NULL) {
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
         // Check for the end of the aditionalInfo section
-        if (strstr(line, "}")) {
+        if (strstr(line, "}"))
+        {
             break;
         }
 
@@ -91,11 +86,13 @@ simple_vector load_primitive_map_aditionalInfo(FILE *file) {
         char key[256];
         char value[256];
 
-        if (sscanf(line, "\"%255[^\"]\" \"%255[^\"]\"", key, value) == 2) {
-
+        if (sscanf(line, "\"%255[^\"]\" \"%255[^\"]\"", key, value) == 2)
+        {
+            
             // Allocate memory for a new MapPropriety
             MapPropriety *prop = malloc(sizeof(MapPropriety));
-            if (!prop) {
+            if (!prop)
+            {
                 // Handle memory allocation failure
                 fprintf(stderr, "Error allocating memory for MapPropriety\n");
                 exit(EXIT_FAILURE);
@@ -114,32 +111,51 @@ simple_vector load_primitive_map_aditionalInfo(FILE *file) {
     return aditionalInfo;
 }
 
+simple_vector load_primitive_map_entitys(FILE *file) {
+    simple_vector entities;
+    entities.data = NULL;
+    entities.size = 0;
 
-// ... (rest of the code)
+    
 
+    return entities;
+}
 
-PrimitiveMap* load_primitive_map(const char *path){
+PrimitiveMap *load_primitive_map(const char *path)
+{
     FILE *file = fopen(path, "r");
-    if (!file) {
+    if (!file)
+    {
         // Handle file open error
         fprintf(stderr, "Error opening file: %s\n", path);
         exit(EXIT_FAILURE);
     }
     PrimitiveMap *ret = malloc(sizeof(PrimitiveMap));
     ret->path = path;
-    ret->entitys = load_primitive_map_entitys(file);
-    ret->structures = load_primitive_map_tructures(file);
+
     ret->aditionalInfo = load_primitive_map_aditionalInfo(file);
+    ret->entitys = load_primitive_map_entitys(file);
+
     return ret;
 }
 
-void delete_primitive_map(PrimitiveMap* map){
+
+
+void delete_primitive_map(PrimitiveMap *map)
+{
     size_t i = 0;
 
-    
+    i = 0;
+    while (i < map->entitys.size)
+    {
+        free(map->entitys.data[i]);
+        i++;
+    }
+    free(map->entitys.data);
 
     i = 0;
-    while(i < map->aditionalInfo.size){
+    while (i < map->aditionalInfo.size)
+    {
         free(map->aditionalInfo.data[i]);
         i++;
     }
@@ -148,21 +164,20 @@ void delete_primitive_map(PrimitiveMap* map){
     free(map);
 }
 
-void print_primitive_map_content(PrimitiveMap *map){
-    printf("{\n");
-    printf("    map path: %s \n",map->path);
+void print_primitive_map_content(PrimitiveMap *map)
+{
+    printf("%s {\n", map->path);
 
     size_t i = 0;
 
-    printf("    map additional info size: %d\n",map->aditionalInfo.size);
-    printf("    map additional info data: {\n");
+    
 
     i = 0;
-    while(i < map->aditionalInfo.size){
+    while (i < map->aditionalInfo.size)
+    {
         MapPropriety *prop = map->aditionalInfo.data[i];
-        printf("        key: %s data: %s \n",prop->key,prop->data);
+        printf("        key: %s data: %s \n", prop->key, prop->data);
         i++;
-
     }
     printf("    }\n");
 
