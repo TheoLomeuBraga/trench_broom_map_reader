bfc1a1843ee3500d3c864cd458341ae195ea6395
TheoLomeuBraga <theolomeu@gmail.com>
2023-12-20 10:07:44 -0300

    a

--- trench_broom_map_reader_brut.h
+++ trench_broom_map_reader_brut.h
@@ -30,8 +30,8 @@ typedef struct
 typedef struct
 {
 
-    const char *key;
-    const char *data;
+    char *key;
+    char *data;
 
 } MapPropriety;
 
@@ -39,7 +39,7 @@ typedef struct
 {
 
     PrimitiveCoord positions[3];
-    const char *texture;
+    char *texture;
     PrimitiveTextureCoord textureCord;
 
 } PrimitiveMapStructurePrimitive;
@@ -98,6 +98,7 @@ simple_vector load_primitive_map_aditionalInfo(FILE *file)
     aditionalInfo.data = NULL;
     aditionalInfo.size = 0;
 
+    
     char line[256];
 
     char first_element_skip = 0;
@@ -133,6 +134,8 @@ simple_vector load_primitive_map_aditionalInfo(FILE *file)
         }
     }
 
+    
+
     return aditionalInfo;
 }
 
@@ -141,28 +144,39 @@ PrimitiveMapStructurePrimitive *read_primitive_map_propriety(const char *data)
     PrimitiveMapStructurePrimitive *primitive = malloc(sizeof(PrimitiveMapStructurePrimitive));
     if (!primitive)
     {
-        // Handle memory allocation failure
         fprintf(stderr, "Error allocating memory for PrimitiveMapStructurePrimitive\n");
         return NULL;
     }
 
     // Attempt to decode the input string
-    int parsed = sscanf(data, " ( %d %d %d ) ( %d %d %d ) ( %d %d %d ) %s %d %d %d %d %d",
+    int parsed = sscanf(data, "( %d %d %d ) ( %d %d %d ) ( %d %d %d ) %ms %d %d %d %d %d",
                         &primitive->positions[0].x, &primitive->positions[0].y, &primitive->positions[0].z,
                         &primitive->positions[1].x, &primitive->positions[1].y, &primitive->positions[1].z,
                         &primitive->positions[2].x, &primitive->positions[2].y, &primitive->positions[2].z,
-                        primitive->texture,
+                        &primitive->texture,
                         &primitive->textureCord.offsetX, &primitive->textureCord.offsetY,
                         &primitive->textureCord.rotation, &primitive->textureCord.scaleX, &primitive->textureCord.scaleY);
 
     if (parsed != 15)
     {
         // If parsing is unsuccessful, free allocated memory and return NULL
+        free(primitive->texture);
         free(primitive);
         return NULL;
     }
 
     return primitive;
+}
+
+
+char is_primitive_entity(const char *line) {
+    // Check if the line contains the beginning of a PrimitiveMapEntity block
+    return strstr(line, "(") != NULL;
+}
+
+char is_map_propriety(const char *line) {
+    // Check if the line contains quoted strings, indicating a MapPropriety
+    return strstr(line, "\"") != NULL;
 }
 
 simple_vector load_primitive_map_entitys(FILE *file)
@@ -171,11 +185,91 @@ simple_vector load_primitive_map_entitys(FILE *file)
     entities.data = NULL;
     entities.size = 0;
 
-    //MapPropriety *read_map_propriety(const char *data)
-    //PrimitiveMapStructurePrimitive *read_primitive_map_propriety(const char *data)
+    char line[256];
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        if (strstr(line, "}")) // Check for the end of the entities section
+        {
+            break;
+        }
+
+        if (is_primitive_entity(line))
+        {
+            PrimitiveMapEntity *entity = malloc(sizeof(PrimitiveMapEntity));
+            if (!entity)
+            {
+                fprintf(stderr, "Error allocating memory for PrimitiveMapEntity\n");
+                exit(EXIT_FAILURE);
+            }
+
+            // Initialize the primitives vector
+            entity->primitives.data = NULL;
+            entity->primitives.size = 0;
+
+            // Initialize the additionalInfo vector
+            entity->aditionalInfo.data = NULL;
+            entity->aditionalInfo.size = 0;
+
+            // Process the current line
+            PrimitiveMapStructurePrimitive *primitive = read_primitive_map_propriety(line);
+            if (primitive)
+            {
+                entity->primitives.data = realloc(entity->primitives.data, sizeof(PrimitiveMapStructurePrimitive *) * (entity->primitives.size + 1));
+                entity->primitives.data[entity->primitives.size++] = primitive;
+                //printf("%d",entity->primitives.size);printf(line);
+            }
+            else
+            {
+                fprintf(stderr, "Error parsing PrimitiveMapStructurePrimitive from line: %s", line);
+                // Handle parsing failure if needed
+            }
+
+            // Continue reading the file for either PrimitiveMapStructurePrimitive or MapPropriety
+            while (fgets(line, sizeof(line), file) != NULL)
+            {
+                if (strstr(line, "}")) // Check for the end of the PrimitiveMapEntity
+                {
+                    break;
+                }
+
+                if (is_primitive_entity(line))
+                {
+                    primitive = read_primitive_map_propriety(line);
+                    if (primitive)
+                    {
+                        entity->primitives.data = realloc(entity->primitives.data, sizeof(PrimitiveMapStructurePrimitive *) * (entity->primitives.size + 1));
+                        entity->primitives.data[entity->primitives.size++] = primitive;
+                        //printf("%d",entity->primitives.size);printf(line);
+                    }
+                    else
+                    {
+                        fprintf(stderr, "Error parsing PrimitiveMapStructurePrimitive from line: %s", line);
+                        // Handle parsing failure if needed
+                    }
+                }
+                else if (is_map_propriety(line))
+                {
+                    // Using read_map_propriety to directly extract quoted strings
+                    MapPropriety *prop = read_map_propriety(line);
+                    if (prop)
+                    {
+                        entity->aditionalInfo.data = realloc(entity->aditionalInfo.data, sizeof(MapPropriety *) * (entity->aditionalInfo.size + 1));
+                        entity->aditionalInfo.data[entity->aditionalInfo.size++] = prop;
+                    }
+                }
+            }
+
+            entities.data = realloc(entities.data, sizeof(PrimitiveMapEntity *) * (entities.size + 1));
+            entities.data[entities.size++] = entity;
+        }
+    }
 
     return entities;
 }
+
+
+
 
 PrimitiveMap *load_primitive_map(const char *path)
 {
@@ -190,7 +284,12 @@ PrimitiveMap *load_primitive_map(const char *path)
     ret->path = path;
 
     ret->aditionalInfo = load_primitive_map_aditionalInfo(file);
+
+    fclose(file);
+    file = fopen(path, "r");
+
     ret->entitys = load_primitive_map_entitys(file);
+    fclose(file);
 
     return ret;
 }
@@ -232,10 +331,21 @@ void print_primitive_map_content(PrimitiveMap *map)
     }
 
     printf("    entity size: %d\n", map->entitys.size);
+
+    
+    printf("    entitys {\n");
+
+    i = 0;
+    while (i < map->entitys.size)
+    {
+        
+        
+        i++;
+    }
+
+    printf("    }\n");
     
     printf("}\n");
 
-    i = 0;
     
-
 }
